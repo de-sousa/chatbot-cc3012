ac07eb3abea4efbaf983d46083b1514702dc925f
aware <aware@localhost.localdomain>
2019-11-12 11:33:57 +0000

    b

--- trial_1.pl
+++ trial_1.pl
@@ -1,38 +1,38 @@
 answers(Words,List):-
     quick_sort(Words,Phrase),
     sentences(Sentences),
-    attribution(Phrase,Sentences,List).
+    attribution(Phrase,Sentences,List),!.
 
 attribution(_,[],[]).
 attribution(Words,[Ph|S],[ans(Ph,Score)|List]):-
     ratio(Words,Ph,Score),
-    attribution(Words,S,List).
+    attribution(Words,S,List),!.
 
 %-------------------------------------
 ratio(Words,Phrase,Score):-
     number(Phrase,X),
     number(Words,Phrase,Y),
-    Score is Y/X.
+    Score is Y/X,!.
 
 number([],0).
 number([_|Xs],S) :-
     number(Xs,Z),
-    S is Z+1.
+    S is Z+1,!.
 
 number([],_,0).
 number(_,[],0).
 number([W|Words],[Ph|Phrase],S):-
-    W=Ph,
+    compare(=,W,Ph),
     number(Words,Phrase,X),
-    S is X+1.
+    S is X+1,!.
 number([W|Words],[Ph|Phrase],S):-
-    W<Ph,
+    compare(<,W,Ph),
     number(Words,[Ph|Phrase],X),
-    S is X.	 
+    S is X,!.	 
 number([W|Words],[Ph|Phrase],S):-
-    W>Ph,
+    compare(>,W,Ph),
     number([W|Words],Phrase,X),
-    S is X.	 
+    S is X,!.	 
         
 %-------------------------------------    
     
@@ -42,14 +42,14 @@ quick_sort([X|Xs],S) :-
     compare_list(<,X,Xs,B),
     quick_sort(A,Y),
     quick_sort(B,Z),
-    append(Y,[X|Z],S).
+    append(Y,[X|Z],S),!.
 
 compare_list(_,_,[],[]).
 compare_list(C,X,[Y|Ys],[Y|S]):-
     compare(C,X,Y),
-    compare_list(C,X,Ys,S).
+    compare_list(C,X,Ys,S),!.
 compare_list(C,X,[Y|Ys],S):-
     not(compare(C,X,Y)),
-    compare_list(C,X,Ys,S).
+    compare_list(C,X,Ys,S),!.
  
  
