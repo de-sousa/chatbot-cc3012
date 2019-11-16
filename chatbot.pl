3257ad90fdd632d281ccb6a9fa5224ab32d52514
Pedro Mota <00motapedro@gmail.com>
2019-11-16 00:01:08 +0000

    database final update for the first checkpoint

--- chatbot.pl
+++ chatbot.pl
@@ -297,7 +297,8 @@ sentences([
      ("are good how ok you","I'm good, and you?"),
      ("are good how ok you","It's everything fine! Thanks for asking! And with you?"),
      ("answer life to","42!"),
-     ("bye","Are you sure you want to leave?"), 
+     ("abort bye leave","Are you sure you want to leave?"),
+     ("example","Can't answer you that, yet. I'm working on it!"),
      %----------------
      ("split_string","split_string(+String,+SepChars,+PadChars,-SubString)\nBreak String into SubStrings.\nThe SepChars argument provides the characters that act as separators and thus the length of SubStrings is one more than the number of separators found if SepChars and PadChars do not have common characters. If SepChars and PadChars are equal, sequences of adjacent separators act as a single separator. Leading and trailing characters for each substring that appear in PadChars are removed from the substring. The input arguments can be either atoms, strings or char/code lists."),
      ("string_split","I think you misspelled the predicate. Try split_string."),
@@ -314,15 +315,71 @@ sentences([
      ("last","last(Xs, LastX)."),
      ("element","element(Index, Integers, Value). % Value is the Indexth element of Integers. Only works with 1-indexed and with integers"),
      ("nth0","nth0(Index, Values, Value). % 0-indexed Values. Value is the Indexth element. See also nth1"),
-     ("length","length(Xs, Count)."),
+     ("length","length(Xs, Count). % Count is the length of the list Xs"),
      ("reverse","reverse(Xs, Reversed)."),
      ("append","append(Xs, Ys, XsThenYs)."),
-     ("flatten","flatten(Nested, Flat). % Remove all but outermost \"[\" and \"]\" from nested list")
-	     ]).
+     ("flatten","flatten(Nested, Flat). % Remove all but outermost \"[\" and \"]\" from nested list"),
+     ("write","write(X). % Write X to the current output."),
+     ("read","read(X). % Read the next Prolog term from the current input stream and unify with X."),
+     ("abs","abs(Expr) % Evaluate Expr and return the absolute value of it"),
+     ("nl","Write a newline to the current output stream."),
+     ("put","put(Char) % Write a Char to the current output stream"),
+     ("tab","tab(N) % Write N spaces on the current output stream"),
+     ("get_char","get_char(Char) % Read the current input stream and unify Char with the next character as a one-character atom."),
+     ("findall","findall(Template,Goal,Bag) % Create a list of the instantiations Template gets successively on backtracking over Goal and unify the result with Bag. Succeeds with an empty list if Goal has no solutions."),
+     ("bagof","bagof(Template,Goal,Bag) % Unify Bag with the alternatives of Template. If Goal has free variables besides the one sharing with Template, bagof/3 will backtrack over the alternatives of these free variables, unifying Bag with the corresponding alternatives of Template."),
+     ("acess_file","access_file(File,Mode) % True if File exists and can be accessed by this Prolog process under mode Mode. Mode is one of the atoms read, write, append, exist, none or execute."),
+     ("exists_file","exists_file(File) % True if File exists and is a regular file. This does not imply the user has read or write access to the file."),
+     ("delete_file","delete_file(File) % Remove File from the file system."),
+     ("rename_file","rename_file(File1,File2) % Rename File1 as File2."),
+     ("size_file","size_file(File,Size) % Unify Size with the size of File in bytes."),
+     ("halt","Terminate Prolog execution."),
+     ("assert","assert(Term) % Assert a clause (fact or rule) into the database.
+     The deprecated assert/1 is equivalent to assertz/1."),
+     ("assertz","assertz(Term) % Asserts the clause as last clause."),
+     ("asserta","asserta(Term) % Asserts the clause as first clause of the predicate."),
+     ("retract","retract(Term) % When Term is an atom or a term it is unified with the first unifying fact or clause in the database. Then, the fact or clause is removed from the database."),
+     ("line_count","line_count(Stream,Count) % Unify Count with the number of lines read or written. Counting starts at 1."),
+     ("character_count","character_count(Stream,Count) % Unify Count with the current character index. For input streams this is the number of characters read since the open; for output streams this is the number of characters written. Counting starts at 0."),
+     ("line_position","line_position(Stream,Count) % Unify Count with the position on the current line. Note that this assumes the position is 0 after the open. Tabs are assumed to be defined on each 8-th character, and backspaces are assumed to reduce the count by one, provided it is positive."),
+     ("garbage_collect","Invoke the global and trail stack garbage collector. Normally the garbage collector is invoked automatically if necessary. Explicit invocation might be useful to reduce the need for garbage collections in time-critical segments of the code. After the garbage collection trim_stacks/0 is invoked to release the collected memory resources."),
+     ("garbage_collect_atoms","Reclaim unused atoms. Normally invoked after agc_margin (a Prolog flag) atoms have been created. On multithreaded versions the actual collection is delayed until there are no threads performing normal garbage collection. In this case garbage_collect_atoms/0 returns immediately. Note that there is no guarantee it will ever happen, as there may always be threads performing garbage collection."),
+     ("garbage_collect_clauses","Reclaim retracted clauses. During normal operation, retracting a clause implies setting the erased generation to the current generation of the database and increment the generation. Keeping the clause around is both needed to realise the logical update view and deal with the fact that other threads may be executing the clause. Both static and dynamic code is processed this way."),
+     ("trim_stacks","Release stack memory resources that are not in use at this moment, returning them to the operating system. It can be used to release memory resources in a backtracking loop, where the iterations require typically seconds of execution time and very different, potentially large, amounts of stack space."),
+     ("shell","shell(Command,Status) % Execute Command on the operating system. Command is given to the Bourne shell (/bin/sh). Status is unified with the exit status of the command."),
+     ("getenv","getenv(Name,Value) % Get environment variable. Fails silently if the variable does not exist. Please note that environment variable names are case-sensitive on Unix systems and case-insensitive on Windows."),
+     ("setenv","setenv(Name,Value) % Set an environment variable. Name and Value must be instantiated to atoms or integers. The environment variable will be passed to shell/[0-2] and can be requested using getenv/2."),
+     ("unsetenv","unsetenv(Name) % Remove an environment variable from the environment. Some systems lack the underlying unsetenv() library function. On these systems unsetenv/1 sets the variable to the empty string."),
+     ("setlocale","setlocale(Category,Old,New) % Set/Query the locale setting which tells the C library how to interpret text files, write numbers, dates, etc. Category is one of all, collate, ctype, messages, monetary, numeric or time. For details, please consult the C library locale documentation."),
+     ("unix","unix(Command) % his predicate comes from the Quintus compatibility library and provides a partial implementation thereof. It provides access to some operating system features and unlike the name suggests, is not operating system specific. Defined Command's are below.
+
+system(+Command)
+    Equivalent to calling shell/1. Use for compatibility only.
+shell(+Command)
+    Equivalent to calling shell/1. Use for compatibility only.
+shell
+    Equivalent to calling shell/0. Use for compatibility only.
+cd
+    Equivalent to calling working_directory/2 to the expansion (see expand_file_name/2) of ~. For compatibility only.
+cd(+Directory)
+    Equivalent to calling working_directory/2. Use for compatibility only.
+argv(-Argv)
+    Unify Argv with the list of command line arguments provided to this Prolog run. Please note that Prolog system arguments and application arguments are separated by --. Integer arguments are passed as Prolog integers, float arguments and Prolog floating point numbers and all other arguments as Prolog atoms. New applications should use the Prolog flag argv. See also the Prolog flag argv."),
+     ("win_exec","win_exec(Command,Show) % Windows only. Spawns a Windows task without waiting for its completion. Show is one of the Win32 SW_* constants written in lowercase without the SW_*: hide maximize minimize restore show showdefault showmaximized showminimized showminnoactive showna shownoactive shownormal. In addition, iconic is a synonym for minimize and normal for shownormal."),
+     ("win_shell","win_shell(Operation,File,Show) % Windows only. Opens the document File using the Windows shell rules for doing so. Operation is one of open, print or explore or another operation registered with the shell for the given document type. On modern systems it is also possible to pass a URL as File, opening the URL in Windows default browser. This call interfaces to the Win32 API ShellExecute(). The Show argument determines the initial state of the opened window (if any). See win_exec/2 for defined values."),
+     ("get_time","get_time(TimeStamp) % Return the current time as a TimeStamp. The granularity is system-dependent."),
+     ("char_conversion","char_conversion(CharIn,CharOut) % Define that term input (see read_term/3) maps each character read as CharIn to the character CharOut."),
+     ("read_term","read_term(Stream,Term,Options) % Read term with options from Stream."),
+     ("is_dict","is_dict(Term) % True if Term is a dict."),
+     ("get_dict","get_dict(Key,Dict,Value) % Unify the value associated with Key in dict with Value. If Key is unbound, all associations in Dict are returned on backtracking. The order in which the associations are returned is undefined. This predicate is normally accessed using the functional notation Dict.Key."),
+     ("select_dict","select_dict(Select,From,Rest) % True when the tags of Select and From have been unified, all keys in Select appear in From and the corresponding values have been unified. The key-value pairs of From that do not appear in Select are used to form an anonymous dict, which us unified with Rest."),
+     ("nth","nth(List,N,Term) % Term is the Nth term in the list."),
+     ("member","member(X,Xs) % True if X is in the list Xs.")
+     	     ]).
 
 ex_sentences(
     [("s sim y yes","Bye!"),
-     ("n não nein no","Thank you! In what can i help you?")
+     ("n não nein no","Thank you! In what can I help you?")
     ]).
 
 
