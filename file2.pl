2f274f04cadea7a699b83754f7c6c7353df442cc
pedroendm <00motapedro@gmail.com>
2019-12-10 17:18:05 +0000

    Correção.

--- file2.pl
+++ file2.pl
@@ -1,11 +1,24 @@
+% ex:
+% sentence_type(["hello","bot"],T).
+% T unifies with "greetings".
+
 sentence_type(L,T) :-
     type(T,L,_).
 
-type(T) --> usefulword(T), uselessword(T).
+type(T) --> [_],type(T),!; usefulword(T),!.
+%-- greetings
 usefulword(T) --> ["hello"], {getType(1,T)}.
-uselessword(T) --> [_], uselessword(T),!.
-uselessword(T) --> [_], usefulword(T),!.
-uselessword(_) --> [].
+usefulword(T) --> ["hi"], {getType(1,T)}.
+%-- help
+usefulword(T) --> ["help"],{getType(2,T)}.
+usefulword(T) --> ["need"],{getType(2,T)}.
+%-- examples
+usefulword(T) --> ["example"], {getType(3,T)}.
+usefulword(T) --> ["ex"], {getType(3,T)}.
+
+
 
 getType(1,"greetings").
+getType(2,"offerhelp").
+getType(3,"offerexample").
     
