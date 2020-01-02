756bb9217d8cbb74c9c200db3aca0820abb5b729
teixeira-de-sousa <teixeira-de-sousa@github.com>
2020-01-02 16:49:42 +0000

    ab

--- file2.pl
+++ file2.pl
@@ -1,14 +1,35 @@
 % ex:
 % sentence_type(["hello","bot"],T).
 % T unifies with "greetings".
-
 sentence_type(L,T) :-
     type(T,L,_).
 
 type(T) -->  usefulword(T),! ; [_],type(T),!.
-%-- greetings
+
 usefulword("greetings") --> ["hello"]; ["hi"].
-%-- help
 usefulword("offerhelp") --> ["help"]; ["need"].
-%-- examples
 usefulword("offerexample") --> ["example"]; ["ex"].
+
+
+% ex:
+% sentence_type("greetings","greetings",C).
+% C unifies with 0.9.
+semtrans(A,B,C):-
+    flow(Xs),
+    member((A,B,C),Xs).
+
+flow([
+	    ("offerhelp","greetings",1),
+	    ("greetings","greetings",1),
+	    (["b","c"],"a",2)
+    ]).
+
+
+
+
+
+take(_,[],[]).
+take(0,_,[]).
+take(N,[X|Xs],[X|Ys]):-
+    M is N-1,
+    take(M,Xs,Ys).
