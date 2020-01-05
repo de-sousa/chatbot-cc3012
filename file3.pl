e8d6ac83fa1addb5bc6c50583fd2db7721723390
teixeira-de-sousa <teixeira-de-sousa@github.com>
2020-01-05 17:19:47 +0000

    ai

--- file3.pl
+++ file3.pl
@@ -42,9 +42,15 @@ type("goodbye") --> ["bye"].
 
 %---------------------------
 
-sumall(_,[],0).
-sumall(A,[(A,_,C)|Flow],Sum):-sumall(A,Flow,S1), Sum is C+S1.
-sumall(A,[(B,_,_)|Flow],Sum):-not(A=B),sumall(A,Flow,Sum).
+listall(_,0,[]).
+listall(A,N,[A|As]):-
+    M is N-1,
+    listall(A,M,As).
+    
+
+sumall(_,[],0,[]).
+sumall(A,[(A,B,C)|Flow],Sum,Cs):-listall((B,C),C,As), append(As,Bs,Cs), sumall(A,Flow,S1,Bs), Sum is C+S1.
+sumall(A,[(B,_,_)|Flow],Sum,Cs):-not(A=B),sumall(A,Flow,Sum,Cs).
 
 choose(A,[(A,B,C)|_],Ca,R,(B,C)):-
     Ca1 is Ca+C,
@@ -60,17 +66,27 @@ choose(A,[(B,_,_)|Flow],Ca,R,(C,D)):-
 % ex:
 % sentence_type("greetings","greetings",C).
 % C unifies with 0.9.
-semtrans(A,B,C):-
+
+semtrans(A,B,Pc):-
+    flow(F),!,
+    sumall(A,F,S,Ls),
+    draw(S,R),
+    nth1(R,Ls,(B,C)),
+    Pc is C/100.0.
+
+semtrans_1(A,B,C):-
     flow(Flow),!,
-    sumall(A,Flow,Sum),
+    sumall(A,Flow,Sum,_),
     not(Sum=0),
     random(0.0,Sum,R),!,
     choose(A,Flow,0,R,(B,C)).
 
 flow([
-	    ([],"greetings",1),
-	    (["greetings","greetings"],"askhelp",1),
-	    (["greetings"],"greetings",1)
+	    ([],"greetings",99),
+	    (["greetings","greetings"],"askhelp",99),
+	    (["greetings"],"greetings",50),
+	    (["greetings"],"askhelp",49),
+	    (_,_,1)
     ]).
 
 %---------------------------
@@ -83,6 +99,9 @@ select(Ts,T) :-
 
 %---------------------------
 
+write_f(A):-
+    write("-"), write_fancy(A).
+
 write_fancy([]):-
     write("\n").
 write_fancy([X|Xs]):-
@@ -90,17 +109,20 @@ write_fancy([X|Xs]):-
     write(X),
     write_fancy(Xs).
 
-chataway_beta(L) :-
-    chat_beta(L,[]). 
-
-chat_beta(0,_) :- sentence_type(A,"goodbye"), write("- "), write_fancy(A),!.
-chat_beta(N,Ms) :- select(Ms,T),!, sentence_type(A,T), write("-"), write_fancy(A), M is N-1, chat_beta(M,[T|Ms]).
-
 chataway(L) :-
-    chat(L,[],_,[]).
+    chat(L,[],_). 
 
-chat(0,_) --> type("goodbye"),!.
-chat(N,Ms) --> {M is N-1, select(Ms,T),!}, type(T), chat(M,[T|Ms]).
+chat(0,_,[A]) :- sentence_type(A,"goodbye"), write_f(A),!.
+chat(N,Ms,[A|As]) :- select(Ms,T),!, sentence_type(A,T), write_f(A), M is N-1, chat(M,[T|Ms],As).
 
-chat_at_aim(From,To,L,_):-
-    chat(L,[],From,To).
+chataway_beta(L) :-
+    chat_beta(L,[],_,[]).
+
+chat_beta(0,_) --> type("goodbye"),!.
+chat_beta(N,Ms) --> {M is N-1, select(Ms,T),!}, type(T), chat_beta(M,[T|Ms]).
+
+aim(To):-
+    chat(_,[],To).
+
+chat_at_aim(From,To,_,_):-
+    aim(To).
