6e5c3cdca23c11b5bdff8929b6ca76a449b11612
teixeira-de-sousa <teixeira-de-sousa@github.com>
2020-01-06 15:19:29 +0000

    ak

--- file3.pl
+++ file3.pl
@@ -53,7 +53,8 @@ nats(N,[N|Xs]) :-
     nats(X,Xs).
 
 draw(N,R) :-
-    nats(N,A), random_permutation(A,B),!, member(R,B).
+    nats(N,A), random_permutation(A,B), member(R,B).
+
 listall(_,0,[]).
 listall(A,N,[A|As]):-
     M is N-1,
@@ -76,8 +77,8 @@ choose(A,[(B,_,_)|Flow],Ca,R,(C,D)):-
     choose(A,Flow,Ca,R,(C,D)).
 
 % ex:
-% sentence_type("greetings","greetings",C).
-% C unifies with 0.9.
+% sentence_type(["greetings"],"greetings",C).
+% C unifies with 75.
 
 semtrans(A,B,Pc):-
     flow(F),!,
@@ -95,20 +96,19 @@ semtrans_1(A,B,C):-
 
 flow([
 	    ([],"greetings",99),
-	    ([],_,0),
-	    (["greetings"],"greetings",75),
-	    (["greetings"],"askhelp",50),
+	    (["greetings"],"greetings",60),
+	    (["greetings"],"askhelp",39),
 	    (["askhelp"],"offerhelp",99),
-	    (["offerhelp"],"proposeanswerquestion",75),
-	    (["proposeanswerquestion","offerhelp"],"goodbye",75),
-	    (["greetings","greetings"],"askhelp",50),
-	    (["greetings","greetings"],"goodbye",2)
+	    (["offerhelp"],"proposeanswerquestion",99),
+	    (["proposeanswerquestion","offerhelp"],"goodbye",99),
+	    (["greetings","greetings"],"askhelp",99),
+	    (_,_,1)
 	]).
 
 %---------------------------
 
 select(Ts,T) :-
-    nats(3,Ns),
+    nats(3,Ns),!,
     member(A,Ns),
     take(A,Ts,X),
     semtrans(X,T,_).
@@ -120,12 +120,12 @@ chataway(L) :-
     chat(L,[],_). 
 
 chat(1,_,[A]) :- sentence_type(A,"goodbye"), write_f(A),!.
-chat(N,Ms,[A|As]) :- select(Ms,T),!, sentence_type(A,T), write_f(A), M is N-1, chat(M,[T|Ms],As).
+chat(N,Ms,[A|As]) :- select(Ms,T),!, sentence_type(A,T), write_f(A), M is N-1,!, chat(M,[T|Ms],As).
 % escolhe o próximo tipo, baseado nos que ja ocorreram: select
 % sentence type gera frase desse tipo
 % write: dá output à frase
 chataway_beta(L) :-
-    chat_beta(L,[],_,[]).
+    chat_beta(L,[],A,[]), write(A).
 
 chat_beta(1,_) --> type("goodbye"),!.
 chat_beta(N,Ms) --> {M is N-1, select(Ms,T),!}, type(T), chat_beta(M,[T|Ms]).
@@ -134,8 +134,8 @@ aim(To,N):-
     length(To,N),
     chat(N,[],To).
 
-chat_at_aim(From,To,M,_):-
-    aim(To,N),
-    nats_inc(M,Ms),
-    member(A,Ms),
+%chat_at_aim(From,To,M,_):-
+%    aim(To,N),
+%    nats_inc(M,Ms),
+%    member(A,Ms).
     
