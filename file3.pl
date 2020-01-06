dd6185611192d728b821516a9775ef0aafe484ac
teixeira-de-sousa <teixeira-de-sousa@github.com>
2020-01-06 15:49:34 +0000

    am

--- file3.pl
+++ file3.pl
@@ -1,4 +1,3 @@
-
 nats_inc(N,Xs):-
     nats(N,Ys),
     reverse(Ys,Xs).
@@ -60,14 +59,6 @@ output(A):-
 
 
 % Auxiliar Predicates
-nats(0,[]).
-nats(N,[N|Xs]) :-
-    not(compare(<,N,1)),
-    X is N-1,
-    nats(X,Xs).
-
-draw(N,R) :-
-    nats(N,A), random_permutation(A,B), member(R,B).
 
 listall(_,0,[]).
 listall(A,N,[A|As]):-
@@ -79,20 +70,9 @@ sumall(_,[],0,[]).
 sumall(A,[(A,B,C)|Flow],Sum,Cs):-listall((B,C),C,As), append(As,Bs,Cs), sumall(A,Flow,S1,Bs), Sum is C+S1.
 sumall(A,[(B,_,_)|Flow],Sum,Cs):-not(A=B),sumall(A,Flow,Sum,Cs).
 
-choose(A,[(A,B,C)|_],Ca,R,(B,C)):-
-    Ca1 is Ca+C,
-    compare(<,R,Ca1).
-choose(A,[(A,_,C)|Flow],Ca,R,(D,E)):-
-    Ca1 is Ca+C,
-    compare(>,R,Ca1),
-    choose(A,Flow,Ca1,R,(D,E)).
-choose(A,[(B,_,_)|Flow],Ca,R,(C,D)):-
-    not(A=B),
-    choose(A,Flow,Ca,R,(C,D)).
-
 % ex:
 % sentence_type(["greetings"],"greetings",C).
-% C unifies with 75.
+% C unifies with 60.
 
 semtrans(A,B,Pc):-
     flow(F),!,
@@ -101,12 +81,6 @@ semtrans(A,B,Pc):-
     nth1(R,Ls,(B,C)),
     Pc is C/100.0.
 
-semtrans_1(A,B,C):-
-    flow(Flow),!,
-    sumall(A,Flow,Sum,_),
-    not(Sum=0),
-    random(0.0,Sum,R),!,
-    choose(A,Flow,0,R,(B,C)).
 
 flow([
 	    ([],"greetings",99),
@@ -119,37 +93,18 @@ flow([
 	    (_,_,1)
 	]).
 
-%---------------------------
-
 select(Ts,T) :-
     nats(3,Ns),!,
     member(A,Ns),
     take(A,Ts,X),
     semtrans(X,T,_).
 
-%---------------------------
 
-
-chataway(L) :-
-    chat(L,[],_). 
-
-chat(1,_,[A]) :- sentence_type(A,"goodbye"), write_f(A),!.
-chat(N,Ms,[A|As]) :- select(Ms,T),!, sentence_type(A,T), write_f(A), M is N-1,!, chat(M,[T|Ms],As).
 % escolhe o próximo tipo, baseado nos que ja ocorreram: select
 % sentence type gera frase desse tipo
 % write: dá output à frase
-chataway_beta(L) :-
-    chat_beta(L,[],A,[]), output(A).
+chataway(L) :-
+    chat(L,[],A,[]),!, output(A).
 
-chat_beta(1,_) --> type("goodbye"),!.
-chat_beta(N,Ms) --> {M is N-1, select(Ms,T),!}, type(T), chat_beta(M,[T|Ms]).
-
-aim(To,N):-
-    length(To,N),
-    chat(N,[],To).
-
-%chat_at_aim(From,To,M,_):-
-%    aim(To,N),
-%    nats_inc(M,Ms),
-%    member(A,Ms).
-    
+chat(1,_) --> type("goodbye"),!.
+chat(N,Ms) --> {M is N-1, select(Ms,T),!}, type(T), chat(M,[T|Ms]).
