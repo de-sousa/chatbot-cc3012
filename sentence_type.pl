7a57ffe9dc11ab42698cabc84d6d5011609445b3
Pedro Mota <00motapedro@gmail.com>
2020-01-06 23:22:09 +0000

    Added some shit

--- sentence_type.pl
+++ sentence_type.pl
@@ -4,10 +4,10 @@ sentence_type(S,T):-
    type(T,S,[]).
 
 % greetings
-type("greetings") -->  {draw(2,R),draw(3,T),draw(4,U)}, hi(R),to(T),extend(U).
+type("greetings") -->  {draw(2,R),draw(3,T),draw(5,U)}, hi(R),to(T),extend(U).
 
-hi(1) --> ["Hi"].
-hi(2) --> ["Hello"].
+hi(1) --> ["Hi,"].
+hi(2) --> ["Hello,"].
 
 to(1) --> ["chatbot!"].
 to(2) --> ["dear"], ["friend."].
@@ -16,6 +16,7 @@ to(3) --> ["friend!"].
 extend(N) --> {0 is N mod 2}, [].
 extend(1) --> ["Hope"],["you"],["are"] ,["having"], ["a"] ,["good"] ,["day!"].
 extend(3) --> ["It's"], ["nice"], ["too"], ["see"], ["you"], ["again!"].
+extend(5) --> ["It"],["feels"],["like"],["I"],["don't"],["see"],["you"],["for"],["years!"].
 
 
 % askhelp
@@ -48,15 +49,6 @@ a(2) --> ["does"], ["the"], ["human"].
 b(1) --> ["the"], ["human"], ["needs"].
 b(2) --> ["you"], ["need"].
 
-% proposeanswerquestion
-type("proposeanswerquestion") --> {draw(5,R)}, question(R).
-question(1) -->  ["What"],["does"],["the"],["append"],["predicate"],["do?\n- append(L1,L2,L3)"], ["L3"],["is"],["the"],["result"],["of"],["the"],["concatenation"],["of"],["L1"],["and"],["L2"].
-question(2) --> ["What"],["does"],["the"],["sort"],["predicate"],["do?\n- sort(L1,L2)"], ["L2"], ["is"], ["the"], ["result"], ["of"], ["sorting"], ["the"], ["list"], ["L1"],["by"],["the"],["natural"], ["order"],["of"],["the"],["elements"].
-question(3) --> ["What"],["does"],["the"],["member"],["predicate"],["do?\n- member(X,L)"], ["is"], ["true"], ["if"], ["X"],["is"],["an"],["element"],["of"],["L"].
-question(4) --> ["What"],["does"],["the"],["select"],["predicate"],["do?\n- select(X,L1,L2)"], ["is"], ["true"], ["if"], ["L2"],["is"],["the"],["list"],["L1"],["without"],["the"],["X"].
-question(5) --> ["What"],["does"],["the"],["prefix"],["predicate"],["do?\n- prefix(Part,Whole"], ["is"], ["true"],["if"],["Part"],["is"],["a"],["leading"],["substring"],["of"],["Whole"].
-question(6) --> ["What"],["does"],["the"],["nth0"],["predicate"],["do?\n- nth0(Id,L,X)"],["is"],["true"],["if"],["the"],["element"],["X"],["is"],["Id'th"],["element"],["of"],["the"],["list."],["Counting"],["starts"],["at"],["zero"]. 
-
 % question
 type("q1") --> ["What does the"],["append"],["predicate do?"].
 type("q2") --> ["What does the"],["sort"],["predicate do?"].
@@ -66,18 +58,21 @@ type("q5") --> ["What does the"],["prefix"],["predicate do?"].
 type("q6") --> ["What does the"],["nth0"],["predicate do?"].
 
 % answer
-type("a1") --> ["append(L1,L2,L3)"], ["L3"],["is"],["the"],["result"],["of"],["the"],["concatenation"],["of"],["L1"],["and"],["L2"].
-type("a2") --> ["sort(L1,L2)"], ["L2"], ["is"], ["the"], ["result"], ["of"], ["sorting"], ["the"], ["list"], ["L1"],["by"],["the"],["natural"], ["order"],["of"],["the"],["elements"].
-type("a3") --> ["member(X,L)"], ["is"], ["true"], ["if"], ["X"],["is"],["an"],["element"],["of"],["L"].
-type("a4") --> ["select(X,L1,L2)"], ["is"], ["true"], ["if"], ["L2"],["is"],["the"],["list"],["L1"],["without"],["the"],["X"].
-type("a5") --> ["prefix(Part,Whole"], ["is"], ["true"],["if"],["Part"],["is"],["a"],["leading"],["substring"],["of"],["Whole"].
-type("a6") --> ["nth0(Id,L,X)"],["is"],["true"],["if"],["the"],["element"],["X"],["is"],["Id'th"],["element"],["of"],["the"],["list."],["Counting"],["starts"],["at"],["zero"].
+type("a1") --> ["append(L1,L2,L3)"], ["L3"],["is"],["the"],["result"],["of"],["the"],["concatenation"],["of"],["L1"],["and"],["L2."].
+type("a2") --> ["sort(L1,L2)"], ["L2"], ["is"], ["the"], ["result"], ["of"], ["sorting"], ["the"], ["list"], ["L1"],["by"],["the"],["natural"], ["order"],["of"],["the"],["elements."].
+type("a3") --> ["member(X,L)"], ["is"], ["true"], ["if"], ["X"],["is"],["an"],["element"],["of"],["L."].
+type("a4") --> ["select(X,L1,L2)"], ["is"], ["true"], ["if"], ["L2"],["is"],["the"],["list"],["L1"],["without"],["the"],["X."].
+type("a5") --> ["prefix(Part,Whole"], ["is"], ["true"],["if"],["Part"],["is"],["a"],["leading"],["substring"],["of"],["Whole."].
+type("a6") --> ["nth0(Id,L,X)"],["is"],["true"],["if"],["the"],["element"],["X"],["is"],["Id'th"],["element"],["of"],["the"],["list."],["Counting"],["starts"],["at"],["zero."].
 
 
 
 
 % thanks
-type("thanks") --> ["Oh,"],["thank"],["you"].
+type("thanks") --> {draw(3,R)}, t(R).
+t(1) --> ["Oh,"],["thank"],["you!"].
+t(2) --> ["Thank"],["you!"],["I"],["didn't"],["use"],["it"],["for"],["a"],["long"],["time,"],["that"],["I"],["forgot."].
+t(3) --> ["Oh,"],["you"],["got"],["me,"],["once"],["again!"],["Thanks."].
 
 % goodbye
 type("goodbye") --> {draw(2,R)}, who(R).
@@ -95,11 +90,12 @@ d(2) --> ["Thankful"], ["for"], ["helping"], ["you!"].
 d(3) --> ["Bye!"], ["Hope"], ["to"], ["see"], ["you"], ["soon."].
 
 % rushedgoodbye
-type("rushedgoodbye") --> {draw(3,R)}, e(R).
+type("rushedgoodbye") --> {draw(4,R)}, e(R).
 
-e(1) --> ["I'm"],["sorry"],["but"],["i"],["need"],["to"],["go..."].
-e(2) --> ["I'm"],["in"],["a"],["hurry..."],["i'm"],["sorry"].
+e(1) --> ["I'm"],["sorry"],["but"],["I"],["need"],["to"],["go..."].
+e(2) --> ["I'm"],["in"],["a"],["hurry..."],["I'm"],["sorry"].
 e(3) --> ["I"], ["don't"], ["have"], ["more"], ["time..."], ["Anyway,"],["thank"],["you!"].
+e(4) --> ["Someone"],["is"],["calling"],["me..."],["It"],["was"],["a"],["pleasure"],["talking"],["to"],["you!"].
 
 
 % auxiliar predicates
