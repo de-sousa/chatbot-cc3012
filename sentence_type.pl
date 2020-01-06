f28f443514636ed86c749fe3bd5e064979a475e4
Pedro Mota <00motapedro@gmail.com>
2020-01-06 14:57:13 +0000

    More improvements to sentence_type

--- sentence_type.pl
+++ sentence_type.pl
@@ -4,23 +4,26 @@ sentence_type(S,T):-
    type(T,S,[]).
 
 % greetings
-type("greetings") -->  {draw(2,r)}, hi(r), {draw(4,t)}, to(t).
+type("greetings") -->  {draw(2,R),draw(3,T),draw(4,U)}, hi(R),to(T),extend(U).
 
-hi(1) --> ["hi"].
-hi(2) --> ["hello"].
+hi(1) --> ["Hi,"].
+hi(2) --> ["Hello,"].
 
-to(1) --> ["chatbot"].
-to(2) --> ["dear"], ["friend"].
-to(3) --> ["friend"].
-to(4) --> [].
+to(1) --> ["chatbot!"].
+to(2) --> ["dear"], ["friend."].
+to(3) --> ["friend!"].
+
+extend(N) --> {0 is N mod 2}, [].
+extend(1) --> ["Hope"],["you"],["are"] ,["having"], ["a"] ,["good"] ,["day!"].
+extend(3) --> ["It's"], ["nice"], ["too"], ["see"], ["you"], ["again!"].
 
 
 % askhelp
-type("askhelp") --> {draw(3,r),draw(4,t)}, modalverb(r),chatbot(t),["help"],["me?"].
+type("askhelp") --> {draw(3,R),draw(4,T)}, modalverb(R),chatbot(T),["help"],["me?"].
 
-modalverb(1) --> ["can"].
-modalverb(2) --> ["could"].
-modalverb(3) --> ["might"].
+modalverb(1) --> ["Can"].
+modalverb(2) --> ["Could"].
+modalverb(3) --> ["Might"].
 
 chatbot(1) --> ["you"].
 chatbot(2) --> ["you,","please,"].
@@ -30,24 +33,20 @@ chatbot(4) --> ["the"], ["chatbot,"], ["please,"].
 
 % offerhelp
 type("offerhelp") -->
-    {draw(2,r), draw(3,t)}, accepting(r), asking(t).
+    {draw(2,R), draw(3,T)}, accepting(R), asking(T).
 
-accepting(1) --> ["of"],["course!"].
-accepting(2) --> ["yes!"].
+accepting(1) --> ["Of"],["course!"].
+accepting(2) --> ["Yes!"].
 
-asking(1) --> {draw(2,r)}, ["in"], ["what"], a(r), ["need"], ["help?"].
-asking(2) --> ["where"], ["can"], ["i"], ["be"], ["useful?"].
-asking(3) --> {draw(2,r)}, ["just"], ["tell"], ["me"], ["what"], b(r), ["to"], ["know."].
+asking(1) --> {draw(2,R)}, ["In"], ["what"], a(R), ["need"], ["help?"].
+asking(2) --> ["Where"], ["can"], ["i"], ["be"], ["useful?"].
+asking(3) --> {draw(2,R)}, ["Just"], ["tell"], ["me"], ["what"], b(R), ["to"], ["know."].
 
 a(1) --> ["do"], ["you"].
 a(2) --> ["does"], ["the"], ["human"].
 
 b(1) --> ["the"], ["human"], ["needs"].
 b(2) --> ["you"], ["need"].
-
-
-% proposequestion
-type("proposequestion") --> ["What"],["does"],["the"],["append"],["predicate"],["do?"].
 
 
 % greetings
@@ -83,7 +82,7 @@ accepting(1) --> ["of"],["course!"].
 accepting(2) --> ["yes!"].
 
 asking(1) --> {draw(2,r)}, ["in"], ["what"], a(r), ["need"], ["help?"].
-asking(2) --> ["where"], ["can"], ["i"], ["be"], ["useful?"].
+asking(2) --> ["where"], ["can"], ["I"], ["be"], ["useful?"].
 asking(3) --> {draw(2,r)}, ["just"], ["tell"], ["me"], ["what"], b(r), ["to"], ["know."].
 
 a(1) --> ["do"], ["you"].
@@ -94,28 +93,37 @@ b(2) --> ["you"], ["need"].
 
 
 % proposeanswerquestion
-type("proposeanswerquestion") -->
-    ["What"],["does"],["the"],["append"],["predicate"],["do?"].
+type("proposeanswerquestion") --> {draw(5,R)}, question(R).
+question(1) -->  ["What"],["does"],["the"],["append"],["predicate"],["do?\n- append(L1,L2,L3)"], ["L3"],["is"],["the"],["result"],["of"],["the"],["concatenation"],["of"],["L1"],["and"],["L2"].
+question(2) --> ["What"],["does"],["the"],["sort"],["predicate"],["do?\n- sort(L1,L2)"], ["L2"], ["is"], ["the"], ["result"], ["of"], ["sorting"], ["the"], ["list"], ["L1"],["by"],["the"],["natural"], ["order"],["of"],["the"],["elements"].
+question(3) --> ["What"],["does"],["the"],["member"],["predicate"],["do?\n- member(X,L)"], ["is"], ["true"], ["if"], ["X"],["is"],["an"],["element"],["of"],["L"].
+question(4) --> ["What"],["does"],["the"],["select"],["predicate"],["do?\n- select(X,L1,L2)"], ["is"], ["true"], ["if"], ["L2"],["is"],["the"],["list"],["L1"],["without"],["the"],["X"].
+question(5) --> ["What"],["does"],["the"],["prefix"],["predicate"],["do?\n- prefix(Part,Whole"], ["is"], ["true"],["if"],["Part"],["is"],["a"],["leading"],["substring"],["of"],["Whole"].
+question(6) --> ["What"],["does"],["the"],["nth0"],["predicate"],["do?\n- nth0(Id,L,X)"],["is"],["true"],["if"],["the"],["element"],["X"],["is"],["Id'th"],["element"],["of"],["the"],["list."],["Counting"],["starts"],["at"],["zero"]. 
+
 
 
 % goodbye
 type("goodbye") --> {draw(2,R)},who(R).
 
-who(1) --> {draw(3,R), draw(3,T)}, c(R), d(T).
-who(2) --> {draw(3,R)}, e(R).
+who(1) --> {draw(4,R)}, c(R).
+who(2) --> {draw(3,R)}, d(R).
 
-c(1) --> ["i'm"],["sorry"],["but"],["i"],["need"],["to"],["go..."].
-c(2) --> ["i'm"],["in"],["a"],["hurry..."],["i'm"],["sorry"].
-c(3) --> ["i"], ["don't"], ["have"], ["more"], ["questions..."].
+c(1) --> ["Thanks"], ["for"], ["the"], ["help!"].
+c(2) --> ["Bye."].
+c(3) --> ["You"], ["did"], ["a"], ["great"], ["job!"], ["bye."].
+c(4) --> ["I"], ["don't"], ["have"], ["more"], ["questions..."], ["Thank"],["you!"].
 
-d(1) --> ["thanks"], ["for"], ["the"], ["help!"].
-d(2) --> ["bye."].
-d(3) --> ["you"], ["did"], ["a"], ["great"], ["job!"], ["bye."].
+d(1) --> ["It"], ["was"], ["a"], ["pleasure"], ["to"], ["help"], ["you!"].
+d(2) --> ["Thankful"], ["for"], ["helping"], ["you!"].
+d(3) --> ["Bye!"], ["Hope"], ["to"], ["see"], ["you"], ["soon."].
 
-e(1) --> ["it"], ["was"], ["a"], ["pleasure"], ["to"], ["help"], ["you!"].
-e(2) --> ["thankful"], ["for"], ["helping"], ["you!"].
-e(3) --> ["bye!"], ["hope"], ["to"], ["see"], ["you"], ["soon."].
-    
+% rushedgoodbye
+type("rushedgoodbye") --> {draw(R,3)}, e(R).
+
+e(1) --> ["I'm"],["sorry"],["but"],["i"],["need"],["to"],["go..."].
+e(2) --> ["I'm"],["in"],["a"],["hurry..."],["i'm"],["sorry"].
+e(3) --> ["I"], ["don't"], ["have"], ["more"], ["time..."], ["Anyway,"],["thank"],["you!"].
 
 
 % auxiliar predicates
@@ -127,3 +135,4 @@ nats(N,[N|Xs]) :-
 
 draw(N,R) :-
     nats(N,A), random_permutation(A,B),!, member(R,B).
+
