8d426321f13ad2d9e0c47b7c66dd476a1d522be5
teixeira-de-sousa <teixeira-de-sousa@github.com>
2020-01-07 01:51:18 +0000

    ay

--- search_procedures.pl
+++ search_procedures.pl
@@ -1,12 +1,14 @@
-bfs(Goal,[[Goal|Path]|_],[Goal|Path],_).
-bfs("goodbye",[["thanks"|Path]|_],["goodbye","thanks"|Path],_).
+bfs(Goal,[[Goal|Path]|_],[Goal|Path],C):-
+    length([Goal|Path],L),
+    not(compare(<,L,C)).
+bfs("goodbye",A,["goodbye"|B],C):-
+    C1 is C-1,
+    bfs("thanks",A,B,C1).
 bfs("rushedgoodbye",[[A]],["rushedgoodbye",A],_).
 bfs(Goal,[Path|Paths],Sol,L) :-
-    not(L=0),
     expand(Path,ExpPaths),
     append(Paths,ExpPaths,Paths2),
-    L1 is L-1,
-    bfs(Goal,Paths2,Sol,L1),!.
+    bfs(Goal,Paths2,Sol,L),!.
 
 expand([First|Path],ExpPaths) :-
     flow(F),
@@ -14,47 +16,3 @@ expand([First|Path],ExpPaths) :-
 	    (member(([First|_],Next,_),F),not(member(Next,[First|Path]))
 	    ),
 	    ExpPaths).
-
-hillclimbing(Goal,h([Goal|Path],_),[Goal|Path]).
-hillclimbing(Goal,h(Path,_),Sol) :-
-    expandh(Path,Goal,HExpPaths),
-    bestpath(HExpPaths,BestPath),
-    hillclimbing(Goal,BestPath,Sol),!.
-
-bestpath([Path],Path).
-bestpath([Path|HPaths],BestPath2) :-
-    bestpath(HPaths,BestPath1),
-    aux_bestpath(Path,BestPath1,BestPath2),!.
-
-aux_bestpath(h(P1,H1),h(_,H2),h(P1,H1)) :- H1>H2,!.
-aux_bestpath(h(_,H1),h(P2,H2),h(P2,H2)) :- H1=<H2,!.
-
-
-bestfirst(Goal,[h([Goal|Path],_)|_],[Goal|Path]).
-bestfirst(Goal,[h(Path,_)|Paths],Sol) :-
-    expandh(Path,Goal,HExpPaths),
-    insert(HExpPaths,Paths,Paths2),
-    bestfirst(Goal,Paths2,Sol),!.
-
-expandh([First|Path],Goal,ExpPaths) :-
-    findall(h([Next,First|Path],H),
-        (transition(First,Next),
-         not(member(Next,[First|Path])),
-         heuristic(Next,Goal,H)),
-        ExpPaths).
-
-heuristic(Next,Goal,H) :-
-    atom_chars(Next,LA),
-    atom_chars(Goal,LB),
-    nequal(LA,LB,H).
-
-insert([],L,L).
-insert([HPath|HPaths],HExpPaths,HExpPaths3) :-
-    aux_insert(HPath,HExpPaths,HExpPaths2),
-    insert(HPaths,HExpPaths2,HExpPaths3),!.
-
-aux_insert(HPath,[],[HPath]).
-aux_insert(h(Path,H),[h(Path2,H2)|HExpPaths],[h(Path,H),h(Path2,H2)|HExpPaths]) :- H>=H2,!.
-aux_insert(h(Path,H),[h(Path2,H2)|HExpPaths],[h(Path2,H2)|HExpPaths2]) :-
-    H<H2,
-    aux_insert(h(Path,H),HExpPaths,HExpPaths2).
